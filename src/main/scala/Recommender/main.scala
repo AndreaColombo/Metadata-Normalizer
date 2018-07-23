f5a7a0c5c36507cbe5f4e9edb09d2e467f629c3d
Andrea Colombo <andrea.colombo.897@gmail.com>
2018-07-23 11:08:14 +0200

    cose

--- src/main/scala/Recommender/main.scala
+++ src/main/scala/Recommender/main.scala
@@ -8,19 +8,21 @@ object main {
 
   def main(args: Array[String]): Unit = {
     if(args.head.equals("score")) {
-      calculate_ontology_score()
-      calculate_score()
+ //     calculate_ontology_score()
+ //     calculate_score()
     }
     else {
       val t = args(0)
       val column_l = config.get_termtype_list(t)
+//      for (col <- column_l) {
+//        println(col)
+//        db_filler.fill_db(t, col)
+//        db_filler.update_db(t, col)
+//      }
+//	db_handler.create_view()
       for (col <- column_l) {
-        println(col)
-        db_filler.fill_db(t, col)
-        db_filler.update_db(t, col)
-      }
-      for (col <- column_l) {
-        calculate_suitability_score(col)
+        //calculate_suitability_score(col)
+	ontologies_set_calculator.calculate_ontology_set(col)
       }
     }
   }
