b3292f809c22a465b36eea7d7978b664a8c43d01
andre <andrea.colombo.897@gmail.com>
2018-07-26 23:24:28 +0200

    onto sets

--- src/main/scala/Recommender/DBCon/db_handler.scala
+++ src/main/scala/Recommender/DBCon/db_handler.scala
@@ -431,7 +431,6 @@ object db_handler {
   def get_score_suitability(onto: String, term_type: String): (Double, Double) = {
     var result: (Double, Double) = (0.0,0.0)
     val db = Database.forConfig("gecotest2", conf)
-    println(onto)
     val q =
       sql"""
            select avg_score1, suitability
