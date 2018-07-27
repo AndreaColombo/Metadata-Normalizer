964a950d1ec9e97f618cfb4898fa1979d573f544
andre <andrea.colombo.897@gmail.com>
2018-07-27 19:11:05 +0200

    Now get info retries 5 times if resource is not available

--- src/main/scala/Enricher/main.scala
+++ src/main/scala/Enricher/main.scala
@@ -65,7 +65,6 @@ object main extends App {
         enrichment_engine.controller(col)
       }
     }
-    Ols_interface.ols_get_info("doid","bbb")
   }
 
   def get_elapsed_time(d1: Long, d2: Long): Unit = {
