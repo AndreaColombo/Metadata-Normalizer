6c52e160c974be44425ede41cc8ff6457764ebd5
Andrea Colombo <andrea.colombo.897@gmail.com>
2018-12-19 14:57:17 +0100

    cose

--- src/main/scala/enricher/dbcon/DbHandler.scala
+++ src/main/scala/enricher/dbcon/DbHandler.scala
@@ -22,7 +22,7 @@ object DbHandler {
   /**
     * Current database
     */
-  private var _db_name = "gecotest_andrea"
+  private var _db_name = "gecotest_new"
 
   /**
     * Getter method for database name
